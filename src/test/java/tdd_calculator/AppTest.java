8d1ab53a61806cdd6a778d892dec3442a0832c09
Andrei_Gordeev <Andrei_Gordeev@epam.com>
2019-12-03 09:55:17 +0300

    initial commit

---
+++ src/test/java/tdd_calculator/AppTest.java
@@ -0,0 +1,20 @@
+package tdd_calculator;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+public class AppTest {
+    private double result;
+    @BeforeClass
+    public void setUp() {
+        result =;
+    }
+
+    @Test
+    public void test_zero() {
+
+    }
+}
