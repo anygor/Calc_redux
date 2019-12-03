22a73c8f9a56b2f6bce2c0582cc14b303e607c17
Andrei_Gordeev <Andrei_Gordeev@epam.com>
2019-12-03 10:07:54 +0300

    zero test

--- src/main/java/tdd_calculator/Calculator.java
+++ src/main/java/tdd_calculator/Calculator.java
@@ -4,8 +4,12 @@ package tdd_calculator;
  * Hello world!
  *
  */
-public class App 
+public class Calculator
 {
+    public static double calculate(String exp){
+        return 0;
+    }
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
--- src/test/java/tdd_calculator/AppTest.java
+++ src/test/java/tdd_calculator/AppTest.java
@@ -1,5 +1,6 @@
 package tdd_calculator;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.BeforeClass;
@@ -7,14 +8,16 @@ import org.junit.Test;
 
 
 public class AppTest {
-    private double result;
+    private double expected;
+    private double actual;
     @BeforeClass
-    public void setUp() {
-        result =;
+    public static void setUp() {
     }
 
     @Test
     public void test_zero() {
-
+        expected = 0;
+        actual = Calculator.calculate("0");
+        assertTrue(expected == actual);
     }
 }
