4c99300ef677f8fed30b90929f57e56eb9f084b8
Andrei_Gordeev <Andrei_Gordeev@epam.com>
2019-12-06 13:54:56 +0300

    calculator_redux_redone

--- src/main/java/com/epam/tddcalculator/Main.java
+++ src/main/java/com/epam/tddcalculator/Main.java
@@ -11,11 +11,11 @@ public class Main {
         String expression;
         log.info("Calculator started.");
         do {
-            log.info("Type an expression you want to calculate. \n Type '-1' to quit.");
+            log.info("Type an expression you want to calculate. \n Type 'quit' to quit.");
             input = new Scanner(System.in);
             expression = input.nextLine();
-            if (expression.equals("-1")) break;
-            log.info("Your expression" + expression);
+            if (expression.equals("quit")) break;
+            log.info("Your expression " + expression);
             log.info(Calculator.calculate(expression));
         } while (true);
     }
